7adcfef081f57cf34256bae811bbe971f94237c6
geverist <geverist@gmail.com>
2025-12-01 10:16:01 -0700

    Update voice handler from workshop

--- handlers/voice-handler.js
+++ handlers/voice-handler.js
@@ -9,15 +9,15 @@ exports.handler = function(context, event, callback) {
   const twilio = require('twilio');
   const twiml = new twilio.twiml.VoiceResponse();
 
-  twiml.say('Hello! Thank you for calling our technical support.', { voice: 'Polly.Joanna' });
+  twiml.say('Hello, this is a call from your medical practice.', { voice: 'Polly.Joanna' });
 
   const gather = twiml.gather({
     numDigits: 1,
     timeout: 10
   });
-  gather.say('Press 1 for software issues. Press 2 for password resets. Press 3 for troubleshooting common problems.', { voice: 'Polly.Joanna' });
+  gather.say('Press 1 to schedule a doctor appointment. Press 2 to cancel an existing appointment. Press 3 to receive an appointment reminder.', { voice: 'Polly.Joanna' });
 
-  twiml.say('If you did not make a selection, we did not receive your input. Please call again. Goodbye!', { voice: 'Polly.Joanna' });
+  twiml.say('Thank you. If you need further assistance, please call back.', { voice: 'Polly.Joanna' });
   twiml.hangup();
 
   callback(null, twiml);
